8221ca852ac828e450ee0a302dce2b47f24e0cb3
Bread-kn72 <ordinarytom72@gmail.com>
2024-04-17 20:54:22 +0900

    Add : 함수 주석 추가

--- 4thAssignment/Model/ProductService.swift
+++ 4thAssignment/Model/ProductService.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class ProductService {
+    // RemoteProduct객체 또는 Error를 매개변수로 받는 함수 생성
     func fetchRemoteProduct(completion: @escaping (Result<RemoteProduct, Error>) -> Void) {
         // 1 ~ 100 사이의 랜덤한 Int 숫자를 가져옵니다.
         let productID = Int.random(in: 1 ... 100)
@@ -16,6 +17,7 @@ class ProductService {
         if let url = URL(string: "https://dummyjson.com/products/\(productID)") {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 if let error = error {
+                    // 오류 발생시 completion 클로저를 통해 .failure 케이스를 전달
                     completion(.failure(error))
                 } else if let data = data {
                     do {
